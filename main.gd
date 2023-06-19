b2d0b7a126de7f213d38d53cb86d819ae5e4d366
krzmig <kuruzumigu@gmail.com>
2023-06-19 20:53:37 +0200

    Fix for the base value of the power of lights and programmatic time lapse added

--- main.gd
+++ main.gd
@@ -12,6 +12,12 @@ const DAYS_IN_YEAR : int = 365
 @export_range( 0.0, HOURS_IN_DAY, 0.0001 ) var day_time : float = 0.0 :
 	set( value ) :
 		day_time = value
+		if day_time < 0.0 :
+			day_time += HOURS_IN_DAY
+			day_of_year -= 1
+		elif day_time > HOURS_IN_DAY :
+			day_time -= HOURS_IN_DAY
+			day_of_year += 1
 		_update()
 @export_range( -90.0, 90.0, 0.01 ) var latitude : float = 0 :
 	set( value ) :
@@ -50,21 +56,45 @@ const DAYS_IN_YEAR : int = 365
 	set( value ) :
 		use_day_time_for_shader = value
 		_update_shader()
+## Multiplier of the elapsed time in the running game.
+## If 0.0 - day time will not be changed automatically.
+## If 1.0 - one hour will take one second.
+@export_range( 0.0, 1.0, 0.0001 ) var time_scale : float = 0.01 :
+	set( value ) :
+		time_scale = value
+## If 0, the value will set from the sun object, but as the script runs in the editor, it may set the wrong value, so it is best to set it manually.
+@export_range( 0.0, 10.0, 0.01 ) var sun_base_enegry : float = 0.0 :
+	set( value ) :
+		sun_base_enegry = value
+		_update_shader()
+## If 0, the value will set from the moon object, but as the script runs in the editor, it may set the wrong value, so it is best to set it manually.
+@export_range( 0.0, 10.0, 0.01 ) var moon_base_enegry : float = 0.0 :
+	set( value ) :
+		moon_base_enegry = value
+		_update_shader()
 
 @onready var environment : WorldEnvironment = $WorldEnvironment
 @onready var sun : DirectionalLight3D = $Sun
-@onready var sun_base_enegry : float = sun.light_energy
 @onready var moon : DirectionalLight3D = $Moon
-@onready var moon_base_enegry : float = moon.light_energy
 
 func _ready() -> void :
-	sun.position = Vector3( 0.0, 0.0, 0.0 )
-	sun.rotation = Vector3( 0.0, 0.0, 0.0 )
-	sun.rotation_order = EULER_ORDER_ZXY
-	moon.position = Vector3( 0.0, 0.0, 0.0 )
-	moon.rotation = Vector3( 0.0, 0.0, 0.0 )
-	moon.rotation_order = EULER_ORDER_ZXY
+	if is_instance_valid( sun ) :
+		sun.position = Vector3( 0.0, 0.0, 0.0 )
+		sun.rotation = Vector3( 0.0, 0.0, 0.0 )
+		sun.rotation_order = EULER_ORDER_ZXY
+		if sun_base_enegry == 0.0 :
+			sun_base_enegry = sun.light_energy
+	if is_instance_valid( moon ) :
+		moon.position = Vector3( 0.0, 0.0, 0.0 )
+		moon.rotation = Vector3( 0.0, 0.0, 0.0 )
+		moon.rotation_order = EULER_ORDER_ZXY
+		if moon_base_enegry == 0.0 :
+			moon_base_enegry = moon.light_energy
 	_update()
+
+func _process( delta: float ) -> void :
+	if not Engine.is_editor_hint() : # We don't want a time lapse in the editor
+		day_time += delta * time_scale
 
 func _update() -> void :
 	_update_sun()
